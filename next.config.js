2bad76791d2b2f66f05b66fae368ef1cf4800413
manbrink <mbrinkma13@gmail.com>
2023-05-02 11:56:13 -0700

    wip landing page

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    domains: ["cards.scryfall.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
